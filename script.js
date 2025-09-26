0ce6f10f89211f924a0b918b6250af140bda78d7
briggsly1 <isaacbrigley@gmail.com>
2025-09-26 12:09:33 -0300

    Create script.js
    Placeholder for price API

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('price-data').innerText = 'USD: $3,650/oz | CAD: $4,950 | AUD: $5,410 (Placeholder)';
+    document.querySelector('#mining-teaser p').innerText = 'Sep 26: Canada One Mining advances Copper Dome (Placeholder)';
+});
